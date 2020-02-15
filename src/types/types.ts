216ccc264548880f32b8f05528465c9efc3e4e91
Leckie <hsk0094@gmail.com>
2020-02-15 20:44:30 +0900

    기본 폰트 추가

---
+++ src/types/types.ts
@@ -0,0 +1,9 @@
+export type COMMON_MOVIE_TYPE = {
+  id: number;
+  title: string;
+  release_data: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  poster_path: string;
+};
