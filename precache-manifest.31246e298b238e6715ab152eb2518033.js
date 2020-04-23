4b0ce6ea68fd23711b60d76c14f923b0c94cb7d3
Leckie <hsk0094@gmail.com>
2020-04-23 22:11:14 +0900

    Updates

---
+++ precache-manifest.31246e298b238e6715ab152eb2518033.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4a5e34f9e3ecec7742bdb9469b29db4",
+    "url": "/movieflex/index.html"
+  },
+  {
+    "revision": "f50a4d7971073379b2a6",
+    "url": "/movieflex/static/js/2.169219ad.chunk.js"
+  },
+  {
+    "revision": "280b48dbc3281f342da16f66e41cf46c",
+    "url": "/movieflex/static/js/2.169219ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a42b2442479468e4264a",
+    "url": "/movieflex/static/js/main.85f0209a.chunk.js"
+  },
+  {
+    "revision": "cdef7fd533430c803ccf",
+    "url": "/movieflex/static/js/runtime-main.cef6d551.js"
+  },
+  {
+    "revision": "21bb70b62317f276f2e97a919ff5bd8c",
+    "url": "/movieflex/static/media/BebasNeue-Regular.21bb70b6.ttf"
+  },
+  {
+    "revision": "c4723de3da750cff5d95b7c9b398d983",
+    "url": "/movieflex/static/media/NotoSansKR-Bold.c4723de3.otf"
+  },
+  {
+    "revision": "1b0504a3e741f8e6e07b297cb8f55d2d",
+    "url": "/movieflex/static/media/NotoSansKR-Light.1b0504a3.otf"
+  },
+  {
+    "revision": "839a2f6c607e26dee10678a077729ba5",
+    "url": "/movieflex/static/media/NotoSansKR-Regular.839a2f6c.otf"
+  }
+]);
